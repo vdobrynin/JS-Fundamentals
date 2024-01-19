9e426bd41f9218ac85e85a3269448aec7d1d7e9e
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-01-18 17:27:50 -0800

    lesson4

--- lessons/lesson4.js
+++ lessons/lesson4.js
@@ -1,26 +1,30 @@
-        // Relational or comparison operators
-
+// Relational or comparison operators
 // > - more than
 // < - less than
-// >= - more than wqual
+// >= - more than equal
 // <= - less than equal
 
-var result1 = 10 > 5
-var result2 = 4 > 5
-console.log(result1)
-console.log(result2)
+// var result1 = 10 > 5
+// console.log(result1)
 
-var result3 = 4 > 4
-console.log(result3)
+// var result2 = 4 > 5
+// console.log(result2)
 
-var result4 = 5 >= 5
-console.log(result4)
-console.log()
+// var result3 = 5 > 5
+// console.log(result3)
 
-        //equality operator
+// var result4 = 4 > 4
+// console.log(result4)
+
+// var result5 = 5 >= 5
+// console.log(result5)
+
+// var result6 = 6 <= 5
+// console.log(result6)
+
+//         //equality operator
 var x = 1
-console.log(x == '1') // lose comparison
-console.log(x === '1') // strict comparison
+console.log(x == '1')   // lose comparison
+console.log(x === '1')  // strict comparison
 
-console.log()
-console.log(x === 1) //
+console.log(x === 1)    // 
