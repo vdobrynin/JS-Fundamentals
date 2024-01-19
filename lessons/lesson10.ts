8e4d157f4be272ef0e92a40af23a4c018776db44
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-01-18 22:54:09 -0800

    lesson10

---
+++ lessons/lesson10.ts
@@ -0,0 +1,12 @@
+
+var customerFirstName: string = 'John'
+var customerLastName: string = 'Smith'
+var customerAge: number = 25
+
+type Customer = { firstName: string, lastName: string, active: boolean }
+
+var firstCustomer: Customer = {
+    firstName: 'Mary',
+    lastName: 'Johns',
+    active: true
+}
