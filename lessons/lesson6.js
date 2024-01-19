32338c88a9f4d19fc22c5b92fcc473f70f1c280d
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-01-18 18:46:49 -0800

    lesson6

--- lessons/lesson6.js
+++ lessons/lesson6.js
@@ -8,20 +8,21 @@
 // if hour between 6 and 12 print "Good Morning"
 // if hour between 12 and 18 print "Good Afternoon"
 // Otherwise: "Good Evening"
+// var hour = 5
+// var hour = 11
+// var hour = 17
+var hour = 20
+// if (hour >= 6 && hour < 12) {
+//     console.log("Good Morning")
+// } else if (hour >= 12 && hour < 18) {
+//     console.log("Good Afternoon")
+// } else {
+//     console.log("Good Evening")
+// }
 
-var hour = 17
-
-if (hour >= 6 && hour < 12) {
-    console.log("Good Morning")
-} else if (hour >= 12 && hour < 18) {
-    console.log("Good Afternoon")
-} else {
-    console.log("Good Evening")
-}
-
+// var ageIsMoreThanEighteen = true
 var ageIsMoreThanEighteen = false
 var isUSCitizen = true
-
 if (ageIsMoreThanEighteen && isUSCitizen) {
     console.log('This customer is eligible for DL')
 } else {
