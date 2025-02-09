b590b5cf70387f0746489e102e6ace537bd4ff71
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-02-09 15:58:42 -0800

    1st review update

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,32 +1,32 @@
 // 1. Hello World
 console.log("Hello World!")
 
-// // Variables
-// var firstName = "John"
-// let lastName = "Smith"
+// Variables
+var firstName = "John"
+let lastName = "Smith"
 
-// console.log(firstName)
-// console.log(lastName)
+console.log(firstName)
+console.log(lastName)
 
-// var age, dateOfBirth, sex
-// age = "5"
-// sex = "Male"
-// console.log(age)
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age)
 
-// age = "6"
-// console.log(age)
+age = "6"
+console.log(age)
 
-// // constants
-// const occupation = "Engineer"
-// // occupation = "driver"         //--> doesn't work, can't change const
-// console.log(occupation)
+// constants
+const occupation = "Engineer"
+// occupation = "driver"         //--> doesn't work, can't change const
+console.log(occupation)
 
-// // data types
-// var middleName = 'David'        //string
-// var ageOfBrother = 25           //number
-// var isHeMarried = false         //boolean
-// var yearsInMarriage = null       //null 
-// var numberOfCars = undefined    //undefined
+// data types
+var middleName = 'David'        //string
+var ageOfBrother = 25           //number
+var isHeMarried = false         //boolean
+var yearsInMarriage = null      //null 
+var numberOfCars = undefined    //undefined
 
 // namespace Solution {
 //   class Solution {
