d19f0bf32785d3388e8cb01416af118d7e384be6
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-02-09 16:15:36 -0800

    2nd lesson review

--- lessons/lesson2.js
+++ lessons/lesson2.js
@@ -8,17 +8,14 @@ var itemName = "Table"
 // var messageToPrint = "The price for your Cup is 50 dollars"
 // console.log(messageToPrint)
 
-// var messageToPrint = "The price for your is 50 dollars"
-// console.log(messageToPrint)
-
 // var messageToPrint = "The price for your" + " is 50 dollars"
 // console.log(messageToPrint)
 
 // var messageToPrint = "The price for your " + itemName + " is 50 dollars"
 // console.log(messageToPrint)
 
-var messageToPrint = "The price for your " + itemName + " is " + price + " dollars" //--> concatenation
+var messageToPrint = "The price for your " + itemName + " is " + price + " dollars"//-->concatenation
 console.log(messageToPrint)
 
-var messageToPrint2 = `The price for your ${itemName} is ${price} dollars`          //--> interpolation
+var messageToPrint2 = `The price for your ${itemName} is ${price} dollars` //--> interpolation
 console.log(messageToPrint2)
