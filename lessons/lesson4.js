28db31216c0ed86d8fc3637a412afb0dca31852a
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-02-09 18:44:59 -0800

    4h lesson review

--- lessons/lesson4.js
+++ lessons/lesson4.js
@@ -1,30 +1,39 @@
 // --> Relational or comparison operators
+
 // > --> more than
 // < --> less than
 // >= --> more than equal
 // <= --> less than equal
 
-// var result1 = 10 > 5
-// console.log(result1)
+var result1 = 10 > 5
+console.log(result1)
 
-// var result2 = 4 > 5
-// console.log(result2)
+var result2 = 4 > 5
+console.log(result2)
 
-// var result3 = 5 > 5
-// console.log(result3)
+var result3 = 4 < 5
+console.log(result3)
+ 
+var result4 = 5 < 5
+console.log(result4)
 
-// var result4 = 4 > 4
-// console.log(result4)
+var result5 = 5 > 5
+console.log(result5) 
 
-// var result5 = 5 >= 5
-// console.log(result5)
+var result6 = 5 >= 5
+console.log(result6) 
 
-// var result6 = 6 <= 5
-// console.log(result6)
+var result7 = 6 >= 5 
+console.log(result7)
+
+var result8 = 4 >= 5
+console.log(result8)
+
+var result9 = 6 <= 5
+console.log(result9)
 
 //         //--> equality operator
 var x = 1
 console.log(x == '1')   //--> lose comparison
 console.log(x === '1')  //--> strict comparison
-
 console.log(x === 1)    // true
