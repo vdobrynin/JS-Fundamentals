b3d08020f3be2cf89bef8d03f17e68d6376fccbf
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-02-10 21:43:05 -0800

    5th lesson review2

--- lessons/lesson5.js
+++ lessons/lesson5.js
@@ -1,23 +1,23 @@
-//        //--> logical "AND" operator
-console.log(true && true) //--> all values have to be TRUE for expression to be TRUE
-console.log(true && false) //--> will be FALSE
+// //        //--> logical "AND" operator
+// console.log(true && true) //--> all values have to be TRUE for expression to be TRUE
+// console.log(true && false) //--> will be FALSE
 
-//        //--> logical "OR" operator
-console.log(true || false) //--> any value should to TRUE for expression to be TRUE
-console.log(false || false) //--> always FALSE
-console.log()
+// //        //--> logical "OR" operator
+// console.log(true || false) //--> any value should to TRUE for expression to be TRUE
+// console.log(false || false) //--> always FALSE
+// console.log()
 
-// var ageIsMoreThanEighteen = true //--> more than or == 16
-var ageIsMoreThanEighteen = false //--> less than 16
-var isUSCitizen = true
-// var isUSCitizen = false
+// // var ageIsMoreThanEighteen = true //--> more than or == 16
+// var ageIsMoreThanEighteen = false //--> less than 16
+// var isUSCitizen = true
+// // var isUSCitizen = false
 
-var eligibilityForDriverLicense = ageIsMoreThanEighteen && isUSCitizen
-console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense)
-console.log()
+// // var eligibilityForDriverLicense = ageIsMoreThanEighteen && isUSCitizen
+// // console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense)
+// // console.log()
 
-var eligibilityForDriverLicense = ageIsMoreThanEighteen || isUSCitizen
-console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense)
+// var eligibilityForDriverLicense = ageIsMoreThanEighteen || isUSCitizen
+// console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense)
 console.log()
 
 //        // --> logical "NOT"
