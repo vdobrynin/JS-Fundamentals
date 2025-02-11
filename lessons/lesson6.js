3883cc87682159bb03f965bc38efa6231fa4d83d
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-02-10 21:46:52 -0800

    6th lesson review

--- lessons/lesson6.js
+++ lessons/lesson6.js
@@ -9,22 +9,26 @@
 // if hour between 12 and 18 print "Good Afternoon"
 // Otherwise: "Good Evening"
 // var hour = 5
+// var hour = 6
 // var hour = 11
-// var hour = 17
-var hour = 20
-// if (hour >= 6 && hour < 12) {
-//     console.log("Good Morning")
-// } else if (hour >= 12 && hour < 18) {
-//     console.log("Good Afternoon")
-// } else {
-//     console.log("Good Evening")
-// }
+var hour = 17
+// var hour = 20
+if (hour >= 6 && hour < 12) {
+    console.log('Good Morning')
+} else if (hour >= 12 && hour < 18) {
+    console.log('Good Afternoon')
+} else {
+    console.log('Good Evening')
+}
+console.log()
 
 // var ageIsMoreThanEighteen = true
 var ageIsMoreThanEighteen = false
 var isUSCitizen = true
+
 if (ageIsMoreThanEighteen && isUSCitizen) {
     console.log('This customer is eligible for DL')
 } else {
     console.log('Customer is NOT eligible for DL')
 }
+console.log()
