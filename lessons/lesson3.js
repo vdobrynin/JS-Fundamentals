f0e60bf2e474d7f579387b2df21aebbfaf0410f0
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-27 21:52:16 -0700

    3d lesson review

--- lessons/lesson3.js
+++ lessons/lesson3.js
@@ -1,16 +1,16 @@
-//objects
+//        // --> objects
 var customer = {
         firstName: 'John',
         lastName: 'Smith',
         cars: ["Volvo", "Toyota", "Tesla"]
 }
-console.log(customer)
+// console.log(customer)
 console.log(customer.firstName)
 console.log(customer.lastName)
+console.log(customer.firstName + ' ' + customer.lastName)
 
 //         //--> dot notation
 customer.firstName = "Mike"
-
 //         //--> bracket notation
 customer['lastName'] = "Silver"
 console.log(customer['firstName'])
@@ -18,12 +18,13 @@ console.log(customer['lastName'])
 //         //-->interpolation
 console.log(`${customer.firstName} ${customer.lastName}`)
 
-//         //--> arrays
+//      //--> arrays
 var car = ["Volvo", "Toyota", "Tesla"]
 console.log(car[0])
 console.log(car[2])
+console.log(car[0] + ' ' + car[2])
 
 car[1] = "BMW"
 console.log(car[1])
 console.log(customer.cars[0])
-
+console.log(car[1] + ' ' + customer.cars[0])
