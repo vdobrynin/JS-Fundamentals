05c014d97ae8f92465cec069896e892c82b525f0
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-27 21:26:36 -0700

    2nd lesson review

--- lessons/lesson2.js
+++ lessons/lesson2.js
@@ -1,21 +1,19 @@
 //Concatenations and Interpolation
+var price1 = 50
+var itemName1 = "Cup"
+var price2 = 80
+var itemName2 = "Table"
+var messageToPrint1 = "The price for your Cup is 50 dollars"
+var messageToPrint2 = "The price for your Table is 80 dollars"
+console.log(messageToPrint1)
+console.log(messageToPrint2)
 
-// var price = 50
-// var itemName = "Cup"
-var price = 80
-var itemName = "Table"
+var messageToPrint = "The price for your" + " is 50 dollars"
+console.log(messageToPrint1)
+var messageToPrint = "The price for your " + itemName1 + " is 50 dollars"
+console.log(messageToPrint1)
 
-// var messageToPrint = "The price for your Cup is 50 dollars"
-// console.log(messageToPrint)
-
-// var messageToPrint = "The price for your" + " is 50 dollars"
-// console.log(messageToPrint)
-
-// var messageToPrint = "The price for your " + itemName + " is 50 dollars"
-// console.log(messageToPrint)
-
-var messageToPrint = "The price for your " + itemName + " is " + price + " dollars"//-->concatenation
-console.log(messageToPrint)
-
-var messageToPrint2 = `The price for your ${itemName} is ${price} dollars` //--> interpolation
+var messageToPrint = "The price for your " + itemName2 + " is " + price2 + " dollars"//-->concatenation
+console.log(messageToPrint2)
+var messageToPrint2 = `The price for your ${itemName2} is ${price2} dollars` //--> interpolation
 console.log(messageToPrint2)
