03ab0cd31987c593c189de93ebe2eb159be5682c
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-28 21:14:07 -0700

    6th lesson review

--- lessons/lesson6.js
+++ lessons/lesson6.js
@@ -1,26 +1,28 @@
-//--> Conditional statement
+//                  // --> Conditional statement
 // if (condition) {
 //     // execute some code here
 // } else {
 //     // execute some code here
 // }
-
 // if hour between 6 and 12 print "Good Morning"
 // if hour between 12 and 18 print "Good Afternoon"
 // Otherwise: "Good Evening"
 // var hour = 5
+// var hour = 3
 // var hour = 6
+// var hour = 12
 // var hour = 11
-var hour = 17
+// var hour = 18
+// var hour = 17
+// var hour = 23
 // var hour = 20
-if (hour >= 6 && hour < 12) {
-    console.log('Good Morning')
-} else if (hour >= 12 && hour < 18) {
-    console.log('Good Afternoon')
-} else {
-    console.log('Good Evening')
-}
-console.log()
+// if (hour >= 6 && hour < 12) {
+//     console.log('Good Morning')
+// } else if (hour >= 12 && hour < 18) {
+//     console.log('Good Afternoon')
+// } else {
+//     console.log('Good Evening')
+// }
 
 // var ageIsMoreThanEighteen = true
 var ageIsMoreThanEighteen = false
@@ -31,4 +33,3 @@ if (ageIsMoreThanEighteen && isUSCitizen) {
 } else {
     console.log('Customer is NOT eligible for DL')
 }
-console.log()
