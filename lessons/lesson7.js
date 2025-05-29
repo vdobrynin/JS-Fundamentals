d6249ae0fbd88abd0e860c72d47cc2d18a6e1e4b
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-29 16:23:50 -0700

    7th loops lesson review

--- lessons/lesson7.js
+++ lessons/lesson7.js
@@ -1,48 +1,40 @@
-// //--> Loops
+//                              // --> Loops
 // console.log('Hello World!')
 // console.log('Hello World!')
 // console.log('Hello World!')
 // for (statement1; statement2; statement3) {
 // }
 
-// //--> for loop (for i loop)
+//                                  // --> for loop (for i loop)
 // for (let i = 0; i < 5; i++) {
 //     // console.log('Hello World!')
 //     console.log('Hello World! ' + i)
 // }
-console.log()
 
 // var cars = ["Volvo", "Toyota", "Tesla"]
 // for (let i = 0; i < cars.length; i++) {
-//     console.log(cars[i])
+//     // console.log(cars[i])
 //     // console.log(cars.length + ' ' + cars[2])
 //     // console.log(cars.length + ' ' + cars[0])
-//     // console.log(cars.length + ' ' + cars[1])
+//     console.log(cars.length + ' ' + cars[1])
 // }
 // console.log()
-
-//        //--> for of loop  --> ***
+//                                // --> for of loop  --> ***
 var cars = ["Volvo", "Toyota", "Tesla"]
 for (let car of cars) {
     console.log(car)
 }
 console.log()
 
-// // for (let c of cars) {
-// //     console.log(c)
-// // }
-// console.log()
+for (let car of cars) {
+    console.log(car)
+    if (car == "Toyota") {
+        break
+    }
+}
+console.log()
 
-// for (let car of cars) {
-//     console.log(car)
-//     if (car == "Toyota") {
-//         break
-//     }
-// }
-// console.log()
-
-//       //--> ES6 syntax for each loop
+// //                   // --> ES6 syntax for each loop
 cars.forEach(car => {
     console.log(car)
 })
-console.log()
