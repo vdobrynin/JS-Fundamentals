c451c436f70c54b8a8d43e2f19e6f52eb4833287
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-11-17 16:14:51 -0800

    add comments in lesson9

--- lessons/lesson9.js
+++ lessons/lesson9.js
@@ -1,7 +1,7 @@
 
-// import { CustomerDetails } from "../helpers/printHelper.js";
-import { customerDetails } from "../helpers/printHelper.js";   // import instance of the class
+// import { CustomerDetails } from "../helpers/printHelper.js"; // instead of importing the entire class
+import { customerDetails } from "../helpers/printHelper.js";   // import instance of the class from above
 
-// var customerDetails = new CustomerDetails()  // remove contractor & to put it into printHelper class
+// var customerDetails = new CustomerDetails() // -->remove contractor & to put it into printHelper class
 customerDetails.printFirstName('Steve')
 customerDetails.printLastName('Smith')     
