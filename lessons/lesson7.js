eb5a36ccf02dabd1f086452afc081e887f4f67a9
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2026-08-18 23:48:36 -0700

    changes in lesson7

--- lessons/lesson7.js
+++ lessons/lesson7.js
@@ -1,4 +1,4 @@
-//                                  // --> for loop (for i loop)
+//                                      // --> for loop (for i loop)
 for (let i = 0; i < 5; i++) {
     // console.log('Hello World!')
     console.log('Hello World! ' + i)
@@ -6,18 +6,17 @@ for (let i = 0; i < 5; i++) {
 
 var cars = ["Volvo", "Toyota", "Tesla"]
 for (let i = 0; i < cars.length; i++) {
-    console.log(cars[i])
-    // console.log(cars.length + ' ' + cars[2])
+    // console.log(cars[i])
+    // console.log(cars[0])
+    console.log(cars.length + ' ' + cars[2])
     // console.log(cars.length + ' ' + cars[0])
     // console.log(cars.length + ' ' + cars[1])
 }
-console.log()
-//                                // --> for of loop  --> ***
-var cars = ["Volvo", "Toyota", "Tesla"]
+
+//                                      // --> for of loop  --> ***
 for (let car of cars) {
     console.log(car)
 }
-console.log()
 
 for (let car of cars) {
     console.log(car)
@@ -25,9 +24,8 @@ for (let car of cars) {
         break
     }
 }
-console.log()
 
-// //                   // --> ES6 syntax for each loop
+//                           // --> ES6 syntax for each loop
 cars.forEach(car => {
     console.log(car)
 })
