677f236534edf8fc02f917df8e8b7b03a1961e85
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2026-08-19 22:32:19 -0700

    changes in lesson9

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,18 +1,22 @@
-export function printAge(age) {     // export to being visible outside #lesson 8
+export function printAge(age) {     // 'export' to being visible outside #lesson 8
     console.log(age)
 }
 
-// class CustomerDetails {          // export to being visible outside, then remove it
-//     printFirstName(firstName) {
-//         console.log(firstName)
-//     }
-//     /**
-//      * This method will print the last name
-//      * @param {string} lastName 
-//      */
-//     printLastName(lastName) {
-//         console.log(lastName) 
-//     }
-// }
+class CustomerDetails {          // 'export' to being visible outside, then remove it
+    /**
+     * This method will print the first name
+     * @param {string} firstName 
+     */
+    printFirstName(firstName) {
+        console.log(firstName)
+    }
+    /**
+     * This method will print the last name
+     * @param {string} lastName 
+     */
+    printLastName(lastName) {
+        console.log(lastName)
+    }
+}
 
-// export const customerDetails = new CustomerDetails() // --> import instance og the class
+export const customerDetails = new CustomerDetails() // --> import instance of the class
--- lessons/lesson9.js
+++ lessons/lesson9.js
@@ -1,7 +1,8 @@
 
 // import { CustomerDetails } from "../helpers/printHelper.js"; // instead of importing the entire class
-import { customerDetails } from "../helpers/printHelper.js";   // import instance of the class from above
+import { customerDetails } from "../helpers/printHelper.js";  // import instance of the class from above
 
-// var customerDetails = new CustomerDetails() // -->remove contractor & to put it into printHelper class
+// var customerDetails = new CustomerDetails() // --> remove contractor & to put it into printHelper class
+
 customerDetails.printFirstName('Steve')
 customerDetails.printLastName('Smith')     
